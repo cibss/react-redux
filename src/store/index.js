99f0bc9c19f2afc82b36a7ff40cb2379b274632b
cibss <cibss13@gmail.com>
2022-08-31 16:35:37 +0700

    auth using redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true
 }
-
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState ,
   reducers: {
     increment(state) {
       state.counter++
@@ -22,11 +21,31 @@ const counterSlice = createSlice({
       state.showCounter = !state.showCounter
     }
   }
+});
+
+const initialAuthState = {
+  isAuth: false
+}
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuth = true
+    },
+    logout(state) {
+      state.isAuth = false
+    }
+  }
 })
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer
+  } 
 })
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
